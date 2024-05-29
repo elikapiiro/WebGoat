c62ae26d275311a9160af1783f0a628180fb20b0
elikapiiro <100676931+elikapiiro@users.noreply.github.com>
2024-05-29 16:36:20 +0300

    Update InsecureDeserializationTask.java

--- src/main/java/org/owasp/webgoat/lessons/deserialization/InsecureDeserializationTask.java
+++ src/main/java/org/owasp/webgoat/lessons/deserialization/InsecureDeserializationTask.java
@@ -82,4 +82,43 @@ public class InsecureDeserializationTask extends AssignmentEndpoint {
     }
     return success(this).build();
   }
+  
+  @PostMapping("/InsecureDeserialization/taskRisky")
+  @ResponseBody
+  public AttackResult completedRiskyTask(@RequestParam String token) throws IOException {
+    String b64token;
+    long before;
+    long after;
+    int delay;
+
+    b64token = token.replace('-', '+').replace('_', '/');
+
+    try (ObjectInputStream ois =
+        new ObjectInputStream(new ByteArrayInputStream(Base64.getDecoder().decode(b64token)))) {
+      before = System.currentTimeMillis();
+      Object o = ois.readObject();
+      if (!(o instanceof VulnerableTaskHolder)) {
+        if (o instanceof String) {
+          return failed(this).feedback("insecure-deserialization.stringobject").build();
+        }
+        return failed(this).feedback("insecure-deserialization.wrongobject").build();
+      }
+      after = System.currentTimeMillis();
+    } catch (InvalidClassException e) {
+      return failed(this).feedback("insecure-deserialization.invalidversion").build();
+    } catch (IllegalArgumentException e) {
+      return failed(this).feedback("insecure-deserialization.expired").build();
+    } catch (Exception e) {
+      return failed(this).feedback("insecure-deserialization.invalidversion").build();
+    }
+
+    delay = (int) (after - before);
+    if (delay > 7000) {
+      return failed(this).build();
+    }
+    if (delay < 3000) {
+      return failed(this).build();
+    }
+    return success(this).build();
+  }
 }
